99143959b3cc65dfae942748620f9c9ae0c2858f
oct0punk <elric.emile@gmail.com>
2021-09-24 17:06:39 +0200

    strLen

--- Cours 0/ApplicationConsole1/ApplicationConsole1/ApplicationConsole1.cpp
+++ Cours 0/ApplicationConsole1/ApplicationConsole1/ApplicationConsole1.cpp
@@ -16,13 +16,21 @@ int Countc(const char* maChaine, char c) {
 	return occur;
 }
 
+int strLen(const char* maChaine) {
+	int count = 0;
+	const char* index = maChaine;
+	while (*index++ != 0)
+		count++;
+	return count;
+}
 
 
 int main()
 {
 	int nbA = Countc("anarchie", 'a');
 
-	printf("nombre de a:%d", nbA);
+	printf("nombre de a:%d\n", nbA);
+	printf("%i", strLen("anarchie"));
 
 	return 0;
 }
