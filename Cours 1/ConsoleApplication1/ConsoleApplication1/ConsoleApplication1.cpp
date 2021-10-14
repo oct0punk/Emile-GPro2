0bbf4dcee60d6d66ae02c48b04a46dd717f469fa
oct0punk <elric.emile@gmail.com>
2021-10-14 17:27:23 +0200

    explicit

--- Cours 1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Cours 1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -40,6 +40,7 @@ const char* StrStr(const char* a, const char* b) {
 
 }
 
+#pragma region RecOps
 int add(int a, int b) {
 	if (b == 0) return a;
 	else
@@ -73,15 +74,139 @@ int divInt(int a, int b) {
 }
 
 int modulo(int a, int b) {
-	if (a == b) return 0;
-	if (b == 1) return 0;
-	if (a < b)
-		return a;
-	if (a > b)
-		return modulo(a - b, b);
+	return mul(sub(a, divInt(a, b)), b);
 }
+#pragma endregion
+
+#pragma region RecStr
+int StrLen(const char* str) {
+	if (*str == 0)
+		return 0;
+	if (!str)
+		return 0;
+	else
+		return 1 + StrLen(str+1);
+}
+
+void StrCpy(char* dest, const char* str) {
+	if (dest == nullptr) return;
+	if (str == nullptr) return;
+	if (*str == 0) return;
+	
+	*dest = *str;
+	dest++;
+	str++;
+	StrCpy(dest, str);
+}
+
+void StrNCpy(char* dest, const char* str, int n) {	
+	if (dest == nullptr) return;
+	if (str == nullptr) return;
+	if (*str == 0) return;
+	if (n < 1) return;
+
+	*dest = *str;
+	dest++;
+	str++;
+	StrNCpy(dest, str, n - 1);
+}
+
+void ZeroMemory(void* ptr, int nbByte) {
+	char* mem = (char*)ptr;
+	if (!mem) return;
+	if (!nbByte) return;
+	*mem = 0;
+	mem++;
+
+	ZeroMemory(mem, nbByte - 1);
+}
+
+void MemCpy(void* dest, const void* src, int nbByte) {
+	if (!src) return;
+	if (nbByte < 1) return;
+	
+	char* bDst = (char*)dest;
+	const char* bSrc = (const char*)src;
+
+	*bDst = *bSrc;
+	MemCpy(bDst + 1, bSrc + 1, nbByte - 1);
+}
+
+const char* StrChrRec(const char* str, char token) {
+	if (!str) return nullptr;
+	if (*str == token)
+		return str;
+	return StrChr(str + 1, token);
+}
+
+bool StrEqual(const char* str, const char* sub) {
+	if (*str != *sub)
+		return false;
+	else
+		if (sub[1] == 0)
+			return true;
+		else
+			return StrEqual(str + 1, sub + 1);
+}
+
+const char* StrStrRec(const char* str, const char* sub) {
+	if (nullptr == str) return nullptr;
+	if (str[0])			return nullptr;
+	if (nullptr == sub) return nullptr;
+
+	if (StrEqual(str, sub))
+		return str;
+	return StrStrRec(str + 1, sub);
+}
+
+char* FindEnd(char* str) {
+	if (str == nullptr) return nullptr;
+	if (str[0]) {
+		if (*str == 0)
+			return str;
+		else
+			return FindEnd(str + 1);
+	}
+}
+
+char* StrCat(char* dest, char* src) {
+	if (nullptr == dest) return nullptr;
+
+	char* dstEnd = FindEnd(dest);
+
+
+}
+
+int StrCmp(const char* a, const char* b) {
+	if (*a == *b) {
+		if (*a == 0) {
+			return 0;
+		}
+		return StrCmp(a + 1, b + 1);
+	}
+	else
+		return *a < *b ? -1 : 1;
+}
+#pragma endregion
+
+#pragma region ExplicitOps
+
+int mulExplicit(int a, int b, int tempResult) {
+	if (b == 0) return tempResult;
+	if (a == 0) return 0;
+
+	if (b > 0)
+		return mulExplicit(a, b - 1, tempResult + a);
+	return mulExplicit(a, b + 1, tempResult - a);
+}
+
+int divIntExplicit(int a, int b, int tempResult) {
+
+}
+#pragma endregion
 
 int main()
 {
-	printf("%i", modulo(10, 2));
+	int jeperdsmeschveux = mulExplicit(4, -8, 0);
+	return 0;
 }
